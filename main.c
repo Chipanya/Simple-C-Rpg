8b2362b5372b0c5a14b2ba72bc1c3b61188232ea
Chipanya <sammw98@gmail.com>
2025-03-08 14:39:00 -0500

    Create menu system

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int displayMenu();
+
 //Create like 3 rooms
 /**
  * have player look around
@@ -7,21 +9,64 @@
  * turn based combat at end, fight dragon?
  */
 
+/*
+just do the fight, 3 rooms ,generate a random enemy type for first 2, then dragon, get an item
+menu at end to add stats change items (equip)
+*/
+
+struct inventory{
+
+};
+
 struct player{
     int health,dmg,strength;
     char hat[25];
 };
+
+struct monster{
+    int health;
+    int strength;
+    int dmg;
+    int expValue;
+};
+
 
 int main(){
 
     printf("Welcome to the Dungeon!\n");
     int choice;
 
-    do{
-        printf("Select an option\n1.\t Look\n2. Open Door\n3.\t Inventory\n4.\t Quit\n");
-        scanf("%d", &choice);
-    }while (choice != 4);
+    do
+    {
+        choice = displayMenu();
+    } while (choice != 4);
+    
     
 
     return 0;
+}
+
+int displayMenu(){
+    int choice;
+
+    printf("Choose an option\n1. Move to next room\n2.Check inventory\n3. Check stats\n4.Check Room Info\n5. Quit\n");
+    scanf("%d", &choice);
+    switch (choice){
+    case 1:
+        printf("go to next room\n");
+        break;
+    case 2:
+        printf("inventory\n");
+        break;
+    case 3:
+        printf("stats\n");
+        break;
+    case 4:
+        printf("Room info\n");
+        break;
+    default:
+        printf("Enter a number between 1 and 5\n");
+        break;
+    }
+    return choice;
 }
